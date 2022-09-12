1dea9fdbe06f6e25520f0d70078413f9617b0e3e
unknown <byiringoroscar@gmail.com>
2022-09-12 12:03:24 +0200

    fix reset password

--- home_view/consumers.py
+++ home_view/consumers.py
@@ -5,19 +5,19 @@ from asgiref.sync import async_to_sync
 
 class NotificationConsumer(WebsocketConsumer):
     def connect(self):
-        self.group_name = 'noti_group_name'
+        self.group_name = 'noti_group_name'   # here we created this group name
         async_to_sync(self.channel_layer.group_add)(
-            self.group_name, self.channel_name
+            self.group_name, self.channel_name    # added group name
         )
         self.accept()
 
     def receive(self, text_data=None, bytes_data=None):
-        self.send(text_data="This is from server!")
+        self.send(text_data="This is from server!")   ## send message to the template to check connection if made
 
     def disconnect(self, code):
-        self.close(code)
+        self.close(code)  ## close connection
 
-    def send_notification(self, event):
+    def send_notification(self, event):   # this is method we created and it is even done in models to get the notifiction in overiding save method and we can even use signals when we wanted it but we used overiding save method
         self.send(event.get(
             'value'))  # this the data we get from Connect model with method type send_notification and now we are
         # going to send it  N:B value is the key we are getting from async_to_sync(channel_layer.group_send) in models so you can call it whatever you want
--- home_view/urls.py
+++ home_view/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from authentication.views import login_user, register_user
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name="home"),
@@ -21,5 +22,12 @@ urlpatterns = [
     # get the notification data
     path('get_notification', views.get_notification, name="get_notification"),
     path('mark_read_notif', csrf_exempt(views.mark_read_notif), name="mark_read_notif"),
-    path('get_all_Notification_count', csrf_exempt(views.get_all_Notification_count), name="get_all_Notification_count")
+    path('get_all_Notification_count', csrf_exempt(views.get_all_Notification_count), name="get_all_Notification_count"),
+
+    # reset password and recovery it
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='reset_password'), # here also we are going to customise our own template
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_sent.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_form.html'), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_done.html'), name='password_reset_complete'),
+
 ]
