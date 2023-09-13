47976f43749d27b9c2eef50fa8f48ca414e3c499
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-09-14 00:21:46 +0200

    Update db

--- home_view/models.py
+++ home_view/models.py
@@ -12,4 +12,4 @@ class Chat(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.user.username}: {self.message}'
+        return f'{self.user.full_name}: {self.message}'
