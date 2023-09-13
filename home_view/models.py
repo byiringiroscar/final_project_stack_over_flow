01d51061d957c12cde7e278bdba8ffda5d3a829f
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-09-14 00:12:03 +0200

    Model for holding chat

--- home_view/models.py
+++ home_view/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
 
 # Create your models here.
+class Chat(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    response = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username}: {self.message}'
