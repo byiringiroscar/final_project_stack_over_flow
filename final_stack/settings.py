31d3b620c669409a86748c924e10439ef67577d0
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-09-19 18:54:47 +0200

    Remove gunicorn version

--- final_stack/settings.py
+++ final_stack/settings.py
@@ -153,7 +153,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [config('redis_url')],
+            "hosts": [(config('redis_url'))],
         },
     },
 }
