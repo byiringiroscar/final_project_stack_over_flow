fc4bd511c77b1f575d7de125d8948f0ef2ae6486
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-20 12:48:28 +0200

    add htmx

--- final_stack/settings.py
+++ final_stack/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'channels',
     'codes.apps.CodesConfig',
     "corsheaders",
+    "django_htmx",
 ]
 
 MIDDLEWARE = [
@@ -59,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = 'final_stack.urls'
