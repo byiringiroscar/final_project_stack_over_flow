26bac08d3aee8d68f34b8529ddb95b14948f8483
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-23 22:43:43 +0200

    wip on sending messages

--- home_view/consumers.py
+++ home_view/consumers.py
@@ -7,6 +7,68 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 User = get_user_model()
 
+class ChatroomConsumer(WebsocketConsumer):
+    def connect(self):
+        self.user = self.scope['user']
+        self.chatroom_name = self.scope['url_route']['kwargs']['chatroom_name']
+        self.chatroom = get_object_or_404(ChatGroup, group_name=self.chatroom_name)
+        async_to_sync(self.channel_layer.group_add)(
+            self.chatroom_name,
+            self.channel_name 
+        )
+        self.accept()
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        body = text_data_json['body']
+        message = GroupMessage.objects.create(
+            author=self.user,
+            group=self.chatroom,
+            body=body
+        )
+        event = {
+            'type': 'message_handler',
+            'message_id': message.id,
+        }
+        async_to_sync(self.channel_layer.group_send)(
+            self.chatroom_name, event
+        )
+        
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.chatroom_name,
+            self.channel_name
+        )
+
+    def message_handler(self, event):
+        message_id = event['message_id']
+        message = GroupMessage.objects.get(id=message_id)
+        context = {
+            'message': message,
+            'user': self.user
+        }
+        html = render_to_string('partials/chat_message_p.html', context=context)
+        self.send(text_data=html) # This will send the message to the browser client but it is not asynous
+    
+    def update_online_count(self):
+        online_count = self.chatroom.users_online.count() -1
+
+        event = {
+            'type': 'online_count_handler',
+            'online_count': online_count
+        }
+
+        async_to_sync(self.channel_layer.group_send)(self.chatroom_name, event)
+
+    
+    def online_count_handler(self, event):
+        online_count = event['online_count']
+        context = {
+            'online_count': online_count
+        }
+        html = render_to_string('partials/online_count.html', context=context)
+        self.send(text_data=html)
+
 
 class ConnectPeopleConsumer(WebsocketConsumer):
     def connect(self):
--- home_view/routing.py
+++ home_view/routing.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .consumers import NotificationConsumer, ConnectPeopleConsumer
+from .consumers import NotificationConsumer, ConnectPeopleConsumer, ChatroomConsumer
 
 ws_patterns = [
     path('ws/notifications/', NotificationConsumer.as_asgi()),
-    path('ws/connect-people', ConnectPeopleConsumer.as_asgi())
+    path('ws/connect-people', ConnectPeopleConsumer.as_asgi()),
+    path('ws/chatroom/<chatroom_name>', ChatroomConsumer.as_asgi()),
 ]
