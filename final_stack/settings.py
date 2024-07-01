c25b17dacd0e334424ed1289026d98b08e68e6ea
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-07-02 00:11:51 +0200

    Update settings.py

--- final_stack/settings.py
+++ final_stack/settings.py
@@ -172,6 +172,15 @@ else:
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
+# TinyMCE Configuration
+TINYMCE_DEFAULT_CONFIG = {
+    'height': 360,
+    'menubar': False,
+    'plugins': 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+    'toolbar': 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+    'content_css': '/static/css/content.css',
+}
+
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
